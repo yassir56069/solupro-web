90c6a1e45c52f8bddeccdc1f7896b399d397c4c2
yassir56069 <hoossanbukshyassir@gmail.com>
2024-11-09 21:44:52 +0400

    added ToS

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -6,16 +6,176 @@ export default function TermsAndConditions() {
 
   return (
     <main>
-        <section className="flex flex-col  text-white mt-10 h-full justify-center items-center text-center">
+        <h1 className="flex font-edgeCutting text-white text-6xl  p-11  justify-center mt-[9rem] "> Terms & Conditions </h1>
+        <div className='flex justify-center items-center '> 
+            <article className="bg-even-darker md:bg-white rounded-xl p-10 md:shadow-md w-[90%] md:w-[80%] m-4 text-white font-creatoDisplay font-thin md:text-blck">
 
+                <ol className="list-decimal space-y-4">
+                    <li>
+                    The Renter accepts the provisions of this rental agreement and has
+                    received a copy of it.
+                    </li>
+                    <li>
+                    The driver shall be at least 21 years of age and shall have held a
+                    driver's license for at least one year before renting a vehicle.
+                    The Renter shall abide by Mauritian law and regulations when
+                    driving. The vehicle is under the responsibility of the Renter,
+                    which, under this rental agreement, can never be less than until
+                    Solupro has registered the vehicle as returned in his system.
+                    Registration of return can only take place at the opening hours.
+                    </li>
+                    <li>
+                    <p>The Renter shall return the vehicle as stated below:</p>
+                    <ol  className=" list-disc ml-10">
+                        <li>
+                        With all attachments, including tires, tools, documents, maps and
+                        other items that were in or on the vehicle upon rental, in the same
+                        condition as upon reception, except for normal wear and tear from
+                        use. If something is missing the Renter agrees that the cost price
+                        of individual items that are absent when the vehicle is returned
+                        will be charged to his or her credit card or cash which the Renter
+                        used at the beginning of the rent. The same applies if extra parts
+                        that came with the vehicle are missing upon the vehicle's return or
+                        if returned in an unsatisfactory condition, i.e. these items will be
+                        charged to the credit card used at the beginning of the rent.
+                        </li>
+                        <li>
+                        At the predetermined time as stated on the front page of the
+                        rental agreement or sooner if Solupro ltd demands it.
+                        </li>
+                        <li>
+                        At the Solupro ltd car rental where the vehicle was rented,
+                        unless otherwise agreed. If at the end of the rental period the vehicle
+                        is not left at Solupro's ltd venue of rent, Solupro ltd is authorized to
+                        charge the renter for pick-up of the vehicle according to its pricelist.
+                        </li>
+                        <li>
+                        With a full tank of fuel. If the vehicle is not returned with a
+                        full fuel tank Solupro is authorized to charge the Renter for the
+                        difference in fuel until the tank is full + a 10 % surcharge at the
+                        pricelist of Solupro.
+                        </li>
+                        <li>
+                        The renter shall return the car in the same given condition of
+                        neatness otherwise the renter will have to pay the cleaning and
+                        overhauling cost.
+                        </li>
+                        <li>
+                        In case of puncture or damage for a reason other than normal water
+                        and tear the Renter agrees to repair or replace it by a tyre of the same
+                        size and similar wear.
+                        </li>
+                    </ol>
+                    </li>
+                    <li>
+                    The Renter shall pay for the fuel and other needed for driving
+                    the vehicle for the period of time the vehicle is at her or his
+                    responsibility, which in the interpretation of this agreement, is never
+                    a shorter period than the period Solupro has to register the vehicle in
+                    its system as returned, which can only be processed during the
+                    company's opening hours.
+                    </li>
+                    <li>
+                    If the Renter does not return the vehicle at the correct time
+                    according to this rental agreement, or negotiates extended rental with
+                    the Solupro ltd office, Solupro ltd or the police are authorized to take
+                    possession of the vehicle without further notice at the Renter's
+                    expense. Extended rental is subject to the consent of Solupro ltd. If
+                    the Renter returns the vehicle 1 hour or more after the expiry of the
+                    rental period, Solupro is authorized to collect as much as a one-day
+                    rent according to this rental agreement. For every day that begins
+                    thereafter, Solupro ltd may collect all charges as per the company's
+                    pricelist.
+                    </li>
+                    <li>
+                    The vehicle shall be cautiously and carefully driven. Only
+                    persons who are registered with Solupro ltd as the drivers and who meet
+                    the provisions of Item 2 above, are authorized to drive the rented
+                    vehicle. If the vehicle is driven by a person   who is not registered in
+                    this rental agreement, all insurance becomes null and void, in which
+                    instance the Renter is fully liable for the vehicle, for damage it may
+                    sustain, damage it may cause others, items or vehicles, and obligates to
+                    pay such damage in full.
+                    </li>
+                    <li>
+                    The Renter has objective liability in respect of the vehicle towards
+                    Solupro, for example, because of damage sustained by Solupro ltd if the
+                    vehicle is stolen or if the vehicle sustains damage that will not be
+                    compensated by the company's insurance company.
+                    </li>
+                    <li>
+                    The Renter has objective liability in respect of Solupro for
+                    damage derived from the use of the vehicle and will not be compensated
+                    by Solupro's ltd insurance company, including damage to passengers or
+                    other persons.
+                    </li>
+                    <li>
+                    <p>The Renter is liable for damage derived from the use of the
+                    vehicle and will not be covered by the vehicle's insurance company,
+                    including damage to the vehicle and/or passengers that may be traced to
+                    the following factors:</p>
+                    <ol type="a" className="list-disc ml-10">
+                        <li>Off-road driving</li>
+                        <li>Driving in rivers or any kind of watercourses</li>
+                        <li>Intentional actions or major negligence</li>
+                        <li>Driver's usage of intoxicants</li>
+                        <li>Usage of the vehicle that is in breach of Mauritian law and/or
+                        the provisions of this rental agreement.</li>
+                    </ol>
+                    </li>
+                    <li>
+                    In the instance of collision or another accident, the Renter
+                    shall immediately notify the police, as well as Solupro ltd. The Renter
+                    may not leave the venue of collision or accident until this has been
+                    done and until the police have arrived, or a damage report has been
+                    prepared. The Renter shall immediately fill out the damage report if
+                    damage has occurred. If the Renter does not report the damage within 12
+                    hours from its occurrence, the Renter is fully liable for the damage and
+                    shall in such instance pay for it in full irrespective of the collision
+                    damage waiver (CDW) that is attached to the insurance taken out by the
+                    Renter at the beginning of the rent.
+                    </li>
+                    <li>
+                    The Renter agrees to pay Solupro ltd a required deposit in the
+                    estimated amount of the rent and/or other charges the Renter may be
+                    subjected to regarding the rent.
+                    </li>
+                    <li>
+                    The Renter is responsible for all parking tickets and fines for
+                    traffic violations. Solupro ltd reserves the right to collect a charge
+                    from the Renter, charged against his or her credit card, according to
+                    Solupro's pricelist, if it turns out that Solupro has to pay fines for
+                    the Renter and/or inform the authorities about the Renter because of
+                    traffic violations.
+                    </li>
+                    <li>
+                    The Renter is responsible for all expenses derived from
+                    transporting the vehicle to Solupro's ltd location, as decided by
+                    Solupro ltd, in the event of transportation because of an accident or
+                    damage to the vehicle, or for other reasons. In such instance the
+                    collision damage waiver (CDW) has no impact.
+                    </li>
+                    <li>
+                    The Renter is not authorized to use the vehicle for transporting
+                    passengers against payment, lend it or sub-lease it.
+                    </li>
+                    <li>
+                    Upon the issue of cyclone warning class 3 the vehicle shall not
+                    circulate otherwise the renter shall be responsible for any damage.
+                    </li>
+                    <li>
+                    The Renter and its travelers are strictly prohibited from smoking
+                    in the car and remain solely liable for any damages or fire caught by
+                    smoking.
+                    </li>
+                    <li>
+                    Solupro ltd reserves the right to cause the car to be verified at
+                    any time.
+                    </li>
+                </ol>
+            </article>
+        </div>
 
-            <h1 className="font-edgeCutting text-6xl  p-8"> Terms & Conditions </h1>
-            
-            <p>
-                text
-            </p>
-        </section>
-        
     </main>
   );
 }
