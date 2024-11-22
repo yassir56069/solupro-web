21696ca050f07e42a9dac6457a64fe4a82fc16dd
yassir56069 <hoossanbukshyassir@gmail.com>
2024-11-22 16:12:02 +0400

    🏗 we need the original domain

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -7,7 +7,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async(subject:any,message:any) => {
     await resend.emails.send({
         to: "hoossanbukshyassir@gmail.com",
-        from: "Solupro <Solupro.mu>",
+        from: "OrcDev <onboarding@resend.dev>",
         subject: subject,
         html: message
     })
