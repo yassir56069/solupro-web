4ccf1bffbbb7dce3cfe2904fc6fc9cceabe1f487
yassir56069 <hoossanbukshyassir@gmail.com>
2024-11-22 16:07:56 +0400

    🧵 back to old test email

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -6,8 +6,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async(subject:any,message:any) => {
     await resend.emails.send({
-        to: "solupro@outlook.fr",
-        from: "OrcDev <onboarding@resend.dev>",
+        to: "hoossanbukshyassir@gmail.com",
+        from: "Solupro <Solupro.mu>",
         subject: subject,
         html: message
     })
