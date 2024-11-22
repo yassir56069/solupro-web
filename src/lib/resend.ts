425d8a6bf2fab97bba0e75715de844a78f3726d9
yassir56069 <hoossanbukshyassir@gmail.com>
2024-11-22 16:19:10 +0400

    📮 should send to client now!

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -6,7 +6,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async(subject:any,message:any) => {
     await resend.emails.send({
-        to: "hoossanbukshyassir@gmail.com",
+        to: "Shameerah.nazeer@gmail.com@",
         from: "OrcDev <onboarding@resend.dev>",
         subject: subject,
         html: message
