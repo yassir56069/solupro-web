a15e2fc44efd693e66e1d5d630f4c690ae69cee2
yassir56069 <hoossanbukshyassir@gmail.com>
2024-11-25 20:24:51 +0400

    🦺fixing env issues (upstash)

--- src/structures/logic-booking-transfer-card.tsx
+++ src/structures/logic-booking-transfer-card.tsx
@@ -116,43 +116,44 @@ export const useBookingTransferCardLogic = () => {
     }
   
     const isBooking = !!formData.pickupLocation;
-      const emailTitle = `Customer ${formData.customerEmail} Tel: ${formData.customerTel} ${isBooking ? `Booking Starting ${range?.start}` : `Transfer Starting ${range?.start}`}`
-  
-      const emailBody = `
-      <div style="font-family: Arial, sans-serif; background-color: #ffffff; padding: 5px;">
-        <div style="
-            display: flex;
-            width: 100%;
-            background: rgb(2,0,36);
-            background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(11,148,121,1) 100%, rgba(0,212,255,1) 100%); 
-            border-radius:6px;
-        "> 
-                <h2 style="color: #ffffff; padding: 4px;">Reservation Details</h2>
-        </div>
-        <p><strong>Customer Email:</strong> ${formData.customerEmail}</p>
-        <p><strong>Customer Phone:</strong> ${formData.customerTel}</p>
-        <p><strong>Reservation Type:</strong> ${isBooking ? "Booking" : "Transfer"}</p>
-        <p><strong>Reservation Dates:</strong> ${range?.start} to ${range?.end}</p>
-        ${isBooking ? `
-          <h3 style="color: #035360;">Booking Details</h3>
-          <p><strong>Pickup Location:</strong> ${formData.pickupLocation}</p>
-          <p><strong>Return Location:</strong> ${formData.returnLocation}</p>
-          <p><strong>Car Type:</strong> 
-            ${selectedSlide === null || selectedSlide === 0 
-              ? 'SUV' 
-              : selectedSlide === 1 
-                ? 'Economy Car' 
-                : 'Pick-Up Truck'}
-          </p>
-        ` : `
-          <h3 style="color: #EE9236;">Transfer Details</h3>
-          <p><strong>Return Location:</strong> ${formData.returnLocation}</p>
-          <p><strong>Number of Passengers:</strong> ${formData.numberPassengers}</p>
-          <p><strong>Number of Baggage:</strong> ${formData.numberBaggage}</p>
-          <p><strong>Flight Arrival Time:</strong> ${flightArrivalTime}</p>
-        `}
+    console.log("KV_REST_API_URL:", process.env.KV_REST_API_URL);
+    const emailTitle = `Customer ${formData.customerEmail} Tel: ${formData.customerTel} ${isBooking ? `Booking Starting ${range?.start}` : `Transfer Starting ${range?.start}`}`
+
+    const emailBody = `
+    <div style="font-family: Arial, sans-serif; background-color: #ffffff; padding: 5px;">
+      <div style="
+          display: flex;
+          width: 100%;
+          background: rgb(2,0,36);
+          background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(11,148,121,1) 100%, rgba(0,212,255,1) 100%); 
+          border-radius:6px;
+      "> 
+              <h2 style="color: #ffffff; padding: 4px;">Reservation Details</h2>
       </div>
-      `;
+      <p><strong>Customer Email:</strong> ${formData.customerEmail}</p>
+      <p><strong>Customer Phone:</strong> ${formData.customerTel}</p>
+      <p><strong>Reservation Type:</strong> ${isBooking ? "Booking" : "Transfer"}</p>
+      <p><strong>Reservation Dates:</strong> ${range?.start} to ${range?.end}</p>
+      ${isBooking ? `
+        <h3 style="color: #035360;">Booking Details</h3>
+        <p><strong>Pickup Location:</strong> ${formData.pickupLocation}</p>
+        <p><strong>Return Location:</strong> ${formData.returnLocation}</p>
+        <p><strong>Car Type:</strong> 
+          ${selectedSlide === null || selectedSlide === 0 
+            ? 'SUV' 
+            : selectedSlide === 1 
+              ? 'Economy Car' 
+              : 'Pick-Up Truck'}
+        </p>
+      ` : `
+        <h3 style="color: #EE9236;">Transfer Details</h3>
+        <p><strong>Return Location:</strong> ${formData.returnLocation}</p>
+        <p><strong>Number of Passengers:</strong> ${formData.numberPassengers}</p>
+        <p><strong>Number of Baggage:</strong> ${formData.numberBaggage}</p>
+        <p><strong>Flight Arrival Time:</strong> ${flightArrivalTime}</p>
+      `}
+    </div>
+    `;
   
     if (!range?.start || !range?.end) {
       setShowRangeError(true);
