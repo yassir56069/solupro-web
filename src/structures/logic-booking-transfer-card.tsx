f069525b938889c8d3cf8aab7e1b6e071301b0cd
yassir56069 <hoossanbukshyassir@gmail.com>
2024-11-25 20:32:32 +0400

    🦺 bypassing constructor (upstash)

--- src/structures/logic-booking-transfer-card.tsx
+++ src/structures/logic-booking-transfer-card.tsx
@@ -88,10 +88,7 @@ export const useBookingTransferCardLogic = () => {
     }));
   };
 
-  const redis = new Redis({
-    url: process.env.KV_REST_API_URL,
-    token: process.env.KV_REST_API_TOKEN,
-  });
+  const redis = Redis.fromEnv()
   
 
   const ratelimit = new Ratelimit({
