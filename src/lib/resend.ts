b9aea0685403a8a13c3b9ce47016168bb2b296dc
yassir56069 <hoossanbukshyassir@gmail.com>
2024-11-30 21:21:04 +0400

    🧵 Email setup with zoho and working domain

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -6,8 +6,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async(subject:any,message:any) => {
     await resend.emails.send({
-        to: "solupro.reservations@gmail.com",
-        from: "solupro.ltd",
+        to: "rentacar@solupro.ltd",
+        from: "rentacar@solupro.ltd",
         subject: subject,
         html: message
     })
