727640188cf8209f5ea55aada9e148d8e5bdd77a
yassir56069 <hoossanbukshyassir@gmail.com>
2024-11-30 21:07:41 +0400

    🧵 chanigng email setup

--- src/lib/resend.ts
+++ src/lib/resend.ts
@@ -6,8 +6,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async(subject:any,message:any) => {
     await resend.emails.send({
-        to: "hoossanbukshyassir@gmail.com",
-        from: "OrcDev <onboarding@resend.dev>",
+        to: "solupro.reservations@gmail.com",
+        from: "solupro.ltd",
         subject: subject,
         html: message
     })
